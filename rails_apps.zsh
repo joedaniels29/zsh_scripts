deeae151574e7b47c7ba949b995709f091de7209
Joseph Daniels <jdanie37@jhmi.edu>
2015-02-03 10:16:15 -0500

    Oh. Just adding more steps to this deployment process

--- rails_apps.zsh
+++ rails_apps.zsh
@@ -10,14 +10,29 @@ rails_apps=(
 deployment_string="group(:production){ gem 'jruby-openssl', '0.8.8';}"
 
 er_deploy(){
-  er_deploymode
+  appname=$(cat config/application.name)
+  if [ -z $appname ]; then
+    echo "please supply the application's name:"
+    read appname
+    echo "Got it! New app name: ${appname}"
+    echo $appname >! config/application.name
+  fi
+
+  er_deploymode > /dev/null
   ember_rails_install $1 --environment=production
-  RAILS_RELATIVE_URL_ROOT="/$APPNAME" RAILS_ENV=production jruby -J-XX:MaxPermSize=256m -S bundle exec rake assets:precompile
+  RAILS_RELATIVE_URL_ROOT="/$appname" RAILS_ENV=production jruby -J-XX:MaxPermSize=256m -S bundle exec rake assets:precompile
   RAILS_ENV=production  warble war
-  chmod +r "$APPNAME.war"
-  mv -v "$APPNAME.war" "$APPNAME.war.`git describe --always`"
+  chmod +r "$appname.war"
+  newappname="$appname.war.`git describe --always`"
+  mv -v "$appname.war" $newappname
   git checkout app/views/pages/_version.html.erb
-  er_devmode
+  er_devmode > /dev/null
+
+  if (($# > 1 )); then
+    scp $newappname $2:/tmp
+  fi
+
+
 }
 er_deploymode(){
   if ! grep -Fxq $deployment_string Gemfile ; then
@@ -28,7 +43,6 @@ er_deploymode(){
 er_devmode(){
   sed -e "s/$deployment_string//g" Gemfile >! _Gemfile;
   mv -f _Gemfile Gemfile
-
   bundle install
 
 }

1b766e5643b7e026efd5778a919c50b1d3cf1d27
Joseph Daniels <jdanie37@jhmi.edu>
2015-02-02 23:58:11 -0500

    fix devmodes/deploymodes so that We dont double the gemfile, or other wierd things like that

--- rails_apps.zsh
+++ rails_apps.zsh
@@ -7,18 +7,28 @@ rails_apps=(
 /Users/josephdaniels/Projects/Work/GSSRegistration/frontend
 )
 
-deployment_string="group :production { gem 'jruby-openssl', '0.8.8' }"
+deployment_string="group(:production){ gem 'jruby-openssl', '0.8.8';}"
 
 er_deploy(){
-  if ! grep -Fxq $deployment_string Gemfile ; then
-    echo $deployment_string >>| Gemfile
-  fi
+  er_deploymode
   ember_rails_install $1 --environment=production
-  RAILS_ENV=production jruby -J-XX:MaxPermSize=256m -S bundle install
   RAILS_RELATIVE_URL_ROOT="/$APPNAME" RAILS_ENV=production jruby -J-XX:MaxPermSize=256m -S bundle exec rake assets:precompile
   RAILS_ENV=production  warble war
   chmod +r "$APPNAME.war"
   mv -v "$APPNAME.war" "$APPNAME.war.`git describe --always`"
   git checkout app/views/pages/_version.html.erb
-  sed -e "s/$deployment_string//g" Gemfile >> Gemfile
+  er_devmode
+}
+er_deploymode(){
+  if ! grep -Fxq $deployment_string Gemfile ; then
+    echo $deployment_string >>| Gemfile
+    RAILS_ENV=production jruby -J-XX:MaxPermSize=256m -S bundle install
+  fi
+}
+er_devmode(){
+  sed -e "s/$deployment_string//g" Gemfile >! _Gemfile;
+  mv -f _Gemfile Gemfile
+
+  bundle install
+
 }

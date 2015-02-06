5eb81f8b652cd9738b6b1f9179b17d16d3e0a100
Joseph Daniels <jdanie37@jhmi.edu>
2015-02-05 22:54:32 -0500

    Uh Oh! Now we have some JXA javascript :o its not just zsh anymore!

---
+++ open_vpn.js
@@ -0,0 +1,22 @@
+function jd_main(){
+  delay(1)
+  var se = Application('System Events')
+  App = Application('Junos Pulse');
+  App.activate();
+  jps = se.processes.byName('Junos Pulse');
+  var menuitem= jps.menuBars[0].menuBarItems.byName("File").menus[0].menuItems[0].menus[0].menuItems[0];
+  if (menuitem.title() !="Connect"){
+    // jps.windows[0].close()
+    return -1
+  }
+
+  menuitem.click()
+
+  //var safari = Application("Safari")
+  //safari.activate()
+  delay(5)
+  sp = se.processes.byName('Safari');
+  se.keyCode(36)
+}
+
+jd_main()

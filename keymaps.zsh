722ecf3e438da492aa4a85a69955ecb21a09855e
Joseph Daniels <jdanie37@jh.edu>
2015-07-04 22:21:15 -0400

    Add some keys

---
+++ keymaps.zsh
@@ -0,0 +1,11 @@
+# Widgetize
+
+
+
+# Bindings
+bindkey -s '\eU' '^Ucd ..; ls^M'
+bindkey -s '\eu' '^Ucd ..; ^M'
+bindkey -s '\el' '^Uls ^M'
+bindkey -s '\es' '^Asudo ^E'
+# refresh current dir
+bindkey -s '\eR' 'cd .^M'

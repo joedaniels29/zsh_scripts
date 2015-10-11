6b146507b717a41d30a946c3cfbce35e21016dfa
Joseph Daniels <jdanie37@jh.edu>
2015-10-11 12:08:04 -0400

    more shortcuts. Add Open zsh_scripts in atom and add an open in Idea.

--- keymaps.zsh
+++ keymaps.zsh
@@ -12,6 +12,11 @@ bindkey -s '\es' '^Asudo ^E'
 bindkey -s '\eR' 'cd .^M rails s ^M'
 # open in atom
 bindkey -s '\eA' 'atom .^M'
+# Open folder in idea
+bindkey -s '\eD' 'idea .^M'
+
 
 # refresh zsh
 bindkey -s '\er\er' 'zsh_reinit;^M'
+# Open ZSH config in atom.
+bindkey -s '\ea\ez¸' 'atom ~/.zsh_scripts;^M'

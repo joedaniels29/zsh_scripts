2d7c0b9350ea97b836e1377ad8d5d4b56552b4b4
Joseph Daniels <jdanie37@jh.edu>
2015-10-11 11:32:36 -0400

    add github scripts

---
+++ github.zsh
@@ -0,0 +1,10 @@
+npm_commit_push_publish(){
+    gh_commit_push_publish $*
+    npm publish ./
+}
+
+gh_commit_push_publish(){
+       git add .
+       git commit -am $1;
+       git push origin master
+ }

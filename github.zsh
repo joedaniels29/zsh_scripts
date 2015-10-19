c0b6f3f415de2bce97f9e8a1c9533dbef5c12d9d
Joseph Daniels <jdanie37@jh.edu>
2015-10-19 17:12:26 -0400

    add new push command

--- github.zsh
+++ github.zsh
@@ -8,6 +8,11 @@ gh_commit_push_publish(){
        git commit -am $1;
        git push origin master
  }
+ git_push_to_origin(){
+       git add .;
+       git commit -am $1;
+       git push origin
+ }
  gh_clone_new(){
         cd ~/Projects/Mine;
         git clone $1;

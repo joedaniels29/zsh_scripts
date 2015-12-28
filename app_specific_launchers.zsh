61cf093e0ca1b677f84850b547cd76dfc02b27ae
Joseph Daniels <jdanie37@jh.edu>
2015-12-28 14:59:40 -0500

    Add *working* launchers

--- app_specific_launchers.zsh
+++ app_specific_launchers.zsh
@@ -1,40 +1,40 @@
 # OASIS Launchers
 sOASe(){
- staServerR "${WORK_PROJECTS_FOLDER}/Oasis" 326
+ sServerE "${WORK_PROJECTS_FOLDER}/Oasis" 326
 }
 
 sOASr(){
- staServerR "${WORK_PROJECTS_FOLDER}/Oasis" 326
+ sServerR "${WORK_PROJECTS_FOLDER}/Oasis" 326
 }
 
 sOAS(){
- staServer "${WORK_PROJECTS_FOLDER}/Oasis" 326
+ sServer "${WORK_PROJECTS_FOLDER}/Oasis" 326
 }
 
 # findings-notification Launchers
 sFNe(){
- staServerR "${WORK_PROJECTS_FOLDER}/findings-notification" 327
+ sServerE "${WORK_PROJECTS_FOLDER}/findings-notification" 327
 }
 
 sFNr(){
- staServerR "${WORK_PROJECTS_FOLDER}/findings-notification" 327
+ sServerR "${WORK_PROJECTS_FOLDER}/findings-notification" 327
 }
 
 sFN(){
- staServer "${WORK_PROJECTS_FOLDER}/findings-notification" 327
+ sServer "${WORK_PROJECTS_FOLDER}/findings-notification" 327
 }
 
 # reach-search Launchers
 sRSe(){
- staServerR "${WORK_PROJECTS_FOLDER}/reach-search" 325
+ sServerE "${WORK_PROJECTS_FOLDER}/reach-search" 325
 }
 
 sRSr(){
- staServerR "${WORK_PROJECTS_FOLDER}/reach-search" 325
+ sServerR "${WORK_PROJECTS_FOLDER}/reach-search" 325
 }
 
 sRS(){
- staServer "${WORK_PROJECTS_FOLDER}/reach-search" 325
+ sServer "${WORK_PROJECTS_FOLDER}/reach-search" 325
 }
 
 sServer(){

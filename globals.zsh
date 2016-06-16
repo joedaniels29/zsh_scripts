18605d0525a8fd22027bcfbf7ed6cbc97a3ef6c8
Joseph Daniels <jdanie37@jh.edu>
2016-06-16 15:54:52 -0400

    fix linux env for harb

--- globals.zsh
+++ globals.zsh
@@ -4,8 +4,12 @@ SCRIPTS_FOLDER="${HOME_FOLDER}/.zsh_scripts"
 JXA_FOLDER="${SCRIPTS_FOLDER}/JXA"
 RUBY_FOLDER="${SCRIPTS_FOLDER}/ruby"
 PROJECTS_FOLDER="${HOME_FOLDER}/Projects"
-WORK_PROJECTS_FOLDER="${PROJECTS_FOLDER}/Work"
 CONTRACT_PROJECTS_FOLDER="${PROJECTS_FOLDER}/Contract"
-
-
 WORK_PROJECTS_FOLDER="${PROJECTS_FOLDER}/Work"
+
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    WORK_PROJECTS_FOLDER="${PROJECTS_FOLDER}/Work"
+elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
+    WORK_PROJECTS_FOLDER="${HOME_FOLDER}/Projects"
+fi

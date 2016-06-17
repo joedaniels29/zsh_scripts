6eb6d7855b2bb34e999a2ef943b178fe7dc56a2a
Joseph Daniels <jdanie37@jh.edu>
2016-06-17 08:52:06 -0400

    ok. the autorun needs more work...

--- init.zsh
+++ init.zsh
@@ -2,16 +2,15 @@ if [[ -e $SCRIPTS_FOLDER/passwords.pw ]]; then
      source "$SCRIPTS_FOLDER/passwords.pw";
 fi
 #
-# if [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then
-#
-#     # if [[ ! -e /tmp/ ]];
+# if [[ "$(expr substr $(uname -ss) 1 5)" -eq  "Linux" ]]; then
+#     autorun "zsh_update" "zsh_update"
 # fi
-
-autorun(){
-    local url="/tmp/d29.zsh.autorun.timestamp.$1"
-    shift;
-    if [[ ! -e $url || $(cat $url) != $(todaysDate) ]]; then
-        eval $*;
-        todaysDate >! "$url"
-    fi
-}
+#
+# autorun(){
+#     local url="/tmp/d29.zsh.autorun.timestamp.$1";
+#     shift;
+#     if [[ ! -e $url || $(cat $url) != $(todaysDate) ]]; then
+#         todaysDate >! "$url";
+#         eval $*;
+#     fi
+# }

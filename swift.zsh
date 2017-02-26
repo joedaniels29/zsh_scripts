8873ad4d1ff2ab4167562668dabf1813e662fbee
Joseph Daniels <jdanie37@jh.edu>
2017-02-25 20:06:37 -0500

    should fix some travis issue

--- swift.zsh
+++ swift.zsh
@@ -7,7 +7,21 @@ isw-print(){
     local pee=$( pwd );
     { echo -n && sleep 3 && echo "import ${pee:t}\n print(\"hi\");\n :print_module ${pee:t}\n" ; cat } | swift -deprecated-integrated-repl   -I $pee
 }
+swift-protobuf-generate(){
+    local pee=$( pwd );
+    { echo -n && sleep 3 && echo "import ${pee:t}\n print(\"hi\");\n :print_module ${pee:t}\n" ; cat } | swift -deprecated-integrated-repl   -I $pee
+}
 
+
+swift_protobuf_generate(){
+    local ppath=$1;
+    local returnTo=$(pwd);
+    cd $ppath;
+    local files=( ./*.pb.swift(N) );
+    (($#files != 0))  && rm -rf  $files;
+    protoc --swift_out=. ./*
+    cd $returnTo;
+}
 
 # Process to generate swift Interface for ruby
 _ruby-interfaceI(){
